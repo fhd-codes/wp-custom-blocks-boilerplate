440fad40b3fac2da98c44764eaa7878d8a8f5cf2
Fahad <fhd1103@outlook.com>
2023-08-09 13:39:31 +0500

    updated

---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd3004e8696be779d3593');
---
+++ build/view.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/view.js ***!
+  \*********************/
+/* eslint-disable no-console */
+console.log('Hello World! (from create-block-boilerplate block)');
+/* eslint-enable no-console */
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
