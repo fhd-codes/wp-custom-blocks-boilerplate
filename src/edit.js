fdc787baea887fba7258c5e260cce0281c4aef84
Fahad <fhd1103@outlook.com>
2023-08-10 15:42:43 +0500

    minor updates

--- src/edit.js
+++ src/edit.js
@@ -1,6 +1,7 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 import './editor.scss';
+import classes from classes
 
 export default function Edit() {
 	return (
--- src/save.js
+++ src/save.js
@@ -1,6 +1,8 @@
 import { useBlockProps } from '@wordpress/block-editor';
+import classes from classes
 
-export default function save() {
+
+export default function Save() {
 	return (
 		<p { ...useBlockProps.save() }>
 			{ 'Boilerplate - hello from the saved content!' }
